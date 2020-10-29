94e465efc610f37ef9f2fca823597c2a6613483a
Ivan <iv.medvedev179@gmail.com>
2020-10-29 21:53:05 +0300

    OOP

---
+++ 04_Functions_and_closures/Without2Zeros.py
@@ -0,0 +1,13 @@
+def No_2Zero(N, K):
+    def f(n):
+        if n == 1:
+            return K - 1
+        elif n == 2:
+            return K ** 2 - K
+        else:
+            return (K-1) * f(n-1) + (K-1) * f(n-2)
+    return f(N)
+        
+            
+            
+#print(No_2Zero(6, 3))
